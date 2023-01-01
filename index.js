07385731901723efecdaf7c71879f3b1903c6f77
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2023-01-01 17:35:38 +0100

    sorting-with-js

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ function getRandomHand() {
 }
 console.log(getRandomHand());
 
- let fruit = [
-  'apple','orange','apple','apple' 
- ]
+let fruit = ['apple', 'orange', 'apple', 'apple', 'orange'];
+let appleShelf = document.querySelector('#apple-shelf');
+let orangeShelf = document.getElementById('orange-shelf');
+console.log(orangeShelf);
+function fruitArrangement() {
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i] === 'apple') {
+      appleShelf.textContent += 'apple' + ' ';
+    } else {
+      orangeShelf.textContent += 'orange' + ' ';
+    }
+  }
+}
+fruitArrangement();
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
